d86cd4dc6bbac9e0294fbe8c16cdae8da79fd80f
Your Real Name <behrizayev@gmail.com>
2023-09-17 19:38:29 +0500

    Deploying to Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn uzshop.wsgi --log-file -
--- uzshop/settings.py
+++ uzshop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-2*)41f*#sop6r&wt(o&)35f&oqcntrux_70d$1@1jm&+ah_+16
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
