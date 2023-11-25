91bcb8e69394641bd25bbb068492eb41cc6f3377
Your Real Name <behrizayev@gmail.com>
2023-11-25 20:41:47 +0500

    updated_statics_new

--- uzshop/settings.py
+++ uzshop/settings.py
@@ -110,19 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-# STATIC_URL = '/static/'
-# MEDIA_URL = 'media/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
 STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Changed from 'static' to 'staticfiles'
-# =======
-# STATIC_URL = 'static/'
-# MEDIA_URL = 'media/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-# >>>>>>> 86d661c24d8c66bc8bcd0bc867997aeb0b29e5d8
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # STATICFILES_DIRS = [
