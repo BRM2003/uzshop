0b37dae52c1fce7d5274695a319a817b7eca0a23
Your Real Name <behrizayev@gmail.com>
2023-11-25 20:10:27 +0500

    updated

--- uzshop/settings.py
+++ uzshop/settings.py
@@ -107,13 +107,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static'
+STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static/",
     os.path.join(BASE_DIR, 'static'), 'static'
 ]
 
