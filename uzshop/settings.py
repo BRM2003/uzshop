29bf46d132a6c1d035b6be9e0322bfe2a4001608
Your Real Name <behrizayev@gmail.com>
2023-11-25 20:45:41 +0500

    updated

--- uzshop/settings.py
+++ uzshop/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2*)41f*#sop6r&wt(o&)35f&oqcntrux_70d$1@1jm&+ah_+16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -110,9 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # STATICFILES_DIRS = [
